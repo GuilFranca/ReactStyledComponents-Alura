d7650528b25b88b81f40e250144bb8e9ccbf6242
Guilherme França <guilfranca27@gmail.com>
2026-08-13 23:51:10 -0300

    Aula 2 - 9. Desafio: banner Finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import styled from "styled-components"
 import EstilosGlobais from "./components/EstilosGlobais";
 import Cabecalho from "./components/Cabecalho";
 import BarraLateral from "./components/BarraLateral";
+import Banner from "./components/Banner";
+import backgroundBanner from "./assets/banner.png"
 
 const FundoGradient = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
+
+  section {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 function App() {
@@ -14,7 +21,14 @@ function App() {
     <FundoGradient>
       <EstilosGlobais />
       <Cabecalho />
-      <BarraLateral />
+      <section>
+        <BarraLateral />
+        
+        <Banner 
+          text={"A galeria mais completa de fotos do espaço!"}
+          backgroundBanner={backgroundBanner}
+        />
+      </section>
     </FundoGradient>
   )
 }
---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const BannerEstilizado = styled.figure`
+    position: relative;
+    border-radius: 20px;
+    width: 1156px;
+    height: 328px;
+    margin: 0;
+
+    img {
+        width: 100%;
+        border-radius: 20px;
+    }
+    
+    p {
+        position: absolute;
+        width: 301px;
+        top: 92px;
+        left: 64px;
+        color: #FFF;
+        font-size: 40px;
+        margin: 0;
+    }
+`
+
+
+const Banner = ({ text, backgroundBanner }) => {
+    return (
+        <BannerEstilizado>
+            <img src={backgroundBanner} alt="banner" />
+            <p>{text}</p>
+        </BannerEstilizado>
+    );
+}
+
+export default Banner;
