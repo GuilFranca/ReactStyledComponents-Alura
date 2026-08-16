9e4faab3fc87ef7921692c94cbc5d443a0c85f8e
Guilherme França <guilfranca27@gmail.com>
2026-08-16 19:44:17 -0300

    Aula 3 - 5. Lidando com as tags Finalizada

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,5 +1,12 @@
+import tags from "./tags.json";
+
 const Tags = () => {
-    return <p>Tags</p>
+    return (
+        <>
+            <p>Busque por tags:</p>
+            {tags.map(tag => <button key={tag.id}>{tag.titulo}</button>)}
+        </>
+    );
 }
 
 export default Tags;
