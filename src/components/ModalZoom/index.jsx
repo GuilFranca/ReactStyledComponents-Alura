1b1a752c169b345154ec6ed978b36758580af275
Guilherme França <guilfranca27@gmail.com>
2026-08-17 23:25:21 -0300

    Aula 4 - 4. Fundo com opacidade Finalizada

--- src/components/ModalZoom/index.jsx
+++ src/components/ModalZoom/index.jsx
@@ -1,12 +1,36 @@
+import styled from "styled-components";
+import Imagem from "../Galeria/Imagem"
+
+const Overlay = styled.div`
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+`;
+
+const DigalogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+`;
+
 const ModalZoom = ({ foto }) => {
     return (
         <>
-            <dialog open={!!foto}>
-                <p>{foto?.titulo}</p>
-                <form method="dialog">
-                    <button type="submit">ok</button>
-                </form>
-            </dialog>
+            {foto &&
+                <Overlay>
+                    <dialog open={!!foto}>
+                        <Imagem
+                            foto={foto}
+                            expandida={true}
+                        />
+                        <form method="dialog">
+                            <button type="submit">ok</button>
+                        </form>
+                    </dialog>
+                </Overlay>
+            }
         </>
     )
 }
