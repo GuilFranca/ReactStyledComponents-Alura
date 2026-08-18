a8569151aeb5554774cb200d370d2b4a3f5418a8
Guilherme França <guilfranca27@gmail.com>
2026-08-18 00:29:16 -0300

    Aula 4 - 5. Desafio: barra de populares e modal estilizada Finalizada do meu jeito

--- src/components/Galeria/Populares/index.jsx
+++ src/components/Galeria/Populares/index.jsx
@@ -1,8 +1,47 @@
+import styled from "styled-components";
 import Titulo from "../../Titulo";
+
+import fotosPopulares from "./fotos-populares.json"
+
+const ContainerPopulares = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const ContainerFotosPopulares = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+
+    img {
+        width: 212px;
+        height: 158px;
+        border-radius: 20px;
+    }
+
+    button {
+        font-size: 20px;
+        width: 208px;
+        height: 56px;
+        border-radius: 10px;
+        border: 2px solid #7B78E5;
+        color: #FFF;
+        background-color: transparent;
+        cursor: pointer;
+    }
+`;
 
 const Populares = () => {
     return (
-        <Titulo $alinhamento={"center"}>Populares</Titulo>
+        <ContainerPopulares>
+            <Titulo $alinhamento={"center"}>Populares</Titulo>
+            <ContainerFotosPopulares>
+                {fotosPopulares.map(fotoPopular => (
+                    <img key={fotoPopular.id} src={fotoPopular.path} alt={fotoPopular.alt} />
+                ))}
+                <button>Ver mais</button>
+            </ContainerFotosPopulares>
+        </ContainerPopulares>
     );
 }
 
